630d32bc794ba6772b7268ee47152c7723febff4
Hu Tianrun <tianrunhu@gmail.com>
2023-11-14 15:00:43 +0800

    refactor

--- src/models/netvlad.py
+++ src/models/netvlad.py
@@ -5,12 +5,12 @@ from sklearn.neighbors import NearestNeighbors
 import numpy as np
 from utils.utils import print_nb_params
 
+
 # based on https://github.com/lyakaap/NetVLAD-pytorch/blob/master/netvlad.py
 class NetVLAD(nn.Module):
     """NetVLAD layer implementation"""
 
-    def __init__(self, num_clusters=64, dim=128, 
-                 normalize_input=True, vladv2=False):
+    def __init__(self, num_clusters=64, dim=128, normalize_input=True, vladv2=False):
         """
         Args:
             num_clusters : int
@@ -34,33 +34,33 @@ class NetVLAD(nn.Module):
         self.centroids = nn.Parameter(torch.rand(num_clusters, dim))
 
     def init_params(self, clsts, traindescs):
-        #TODO replace numpy ops with pytorch ops
+        # TODO replace numpy ops with pytorch ops
         if self.vladv2 == False:
             clstsAssign = clsts / np.linalg.norm(clsts, axis=1, keepdims=True)
             dots = np.dot(clstsAssign, traindescs.T)
             dots.sort(0)
-            dots = dots[::-1, :] # sort, descending
+            dots = dots[::-1, :]  # sort, descending
 
-            self.alpha = (-np.log(0.01) / np.mean(dots[0,:] - dots[1,:])).item()
+            self.alpha = (-np.log(0.01) / np.mean(dots[0, :] - dots[1, :])).item()
             self.centroids = nn.Parameter(torch.from_numpy(clsts))
-            self.conv.weight = nn.Parameter(torch.from_numpy(self.alpha*clstsAssign).unsqueeze(2).unsqueeze(3))
+            self.conv.weight = nn.Parameter(
+                torch.from_numpy(self.alpha * clstsAssign).unsqueeze(2).unsqueeze(3)
+            )
             self.conv.bias = None
         else:
-            knn = NearestNeighbors(n_jobs=-1) #TODO faiss?
+            knn = NearestNeighbors(n_jobs=-1)  # TODO faiss?
             knn.fit(traindescs)
             del traindescs
             dsSq = np.square(knn.kneighbors(clsts, 2)[1])
             del knn
-            self.alpha = (-np.log(0.01) / np.mean(dsSq[:,1] - dsSq[:,0])).item()
+            self.alpha = (-np.log(0.01) / np.mean(dsSq[:, 1] - dsSq[:, 0])).item()
             self.centroids = nn.Parameter(torch.from_numpy(clsts))
             del clsts, dsSq
 
             self.conv.weight = nn.Parameter(
                 (2.0 * self.alpha * self.centroids).unsqueeze(-1).unsqueeze(-1)
             )
-            self.conv.bias = nn.Parameter(
-                - self.alpha * self.centroids.norm(dim=1)
-            )
+            self.conv.bias = nn.Parameter(-self.alpha * self.centroids.norm(dim=1))
 
     def forward(self, x):
         N, C = x.shape[:2]
@@ -73,21 +73,27 @@ class NetVLAD(nn.Module):
         soft_assign = F.softmax(soft_assign, dim=1)
 
         x_flatten = x.view(N, C, -1)
-        
+
         # calculate residuals to each clusters
-        vlad = torch.zeros([N, self.num_clusters, C], dtype=x.dtype, layout=x.layout, device=x.device)
-        for C in range(self.num_clusters): # slower than non-looped, but lower memory usage 
-            residual = x_flatten.unsqueeze(0).permute(1, 0, 2, 3) - \
-                    self.centroids[C:C+1, :].expand(x_flatten.size(-1), -1, -1).permute(1, 2, 0).unsqueeze(0)
-            residual *= soft_assign[:,C:C+1,:].unsqueeze(2)
-            vlad[:,C:C+1,:] = residual.sum(dim=-1)
+        vlad = torch.zeros(
+            [N, self.num_clusters, C], dtype=x.dtype, layout=x.layout, device=x.device
+        )
+        for C in range(
+            self.num_clusters
+        ):  # slower than non-looped, but lower memory usage
+            residual = x_flatten.unsqueeze(0).permute(1, 0, 2, 3) - self.centroids[
+                C : C + 1, :
+            ].expand(x_flatten.size(-1), -1, -1).permute(1, 2, 0).unsqueeze(0)
+            residual *= soft_assign[:, C : C + 1, :].unsqueeze(2)
+            vlad[:, C : C + 1, :] = residual.sum(dim=-1)
 
         vlad = F.normalize(vlad, p=2, dim=2)  # intra-normalization
         vlad = vlad.view(x.size(0), -1)  # flatten
         vlad = F.normalize(vlad, p=2, dim=1)  # L2 normalize
 
         return vlad
-    
+
+
 def main():
     x = torch.randn(1, 1024, 20, 20)
     netvlad = NetVLAD(num_clusters=64, dim=1024)
@@ -95,6 +101,7 @@ def main():
     # torch.Size([1, 4096])
     print_nb_params(netvlad)
     # Trainable parameters: 0.262M
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 
+
 def print_nb_params(m):
     model_parameters = filter(lambda p: p.requires_grad, m.parameters())
     params = sum([np.prod(p.size()) for p in model_parameters])
-    print(f'Trainable parameters: {params/1e6:.3}M')
+    print(f"Trainable parameters: {params/1e6:.3}M")
