a55c2a2685d725a8ee7d71e8d8d91ee800e0fccf
Hu Tianrun <tianrunhu@gmail.com>
2023-11-15 14:22:02 +0800

    add files

--- src/evaluation/test.py
+++ src/evaluation/test.py
@@ -1,6 +1,11 @@
+import torch
 from torch.utils.data import Dataset
 from torch import nn
+import logging
 
 
 def test(eval_ds: Dataset, model: nn.Module):
-    pass
+    model = model.eval()
+    with torch.no_grad():
+        logging.info("Extracting database descriptors...")
+        
